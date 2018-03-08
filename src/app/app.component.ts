bd9578a5faa50051e06c932b7b115b1c3d8d13fd
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2018-03-08 07:18:40 -0600

    Added patterns for input and form validation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular 5 Sandbox';
+  username = 'Srini';
+
+  onSubmit(formVal) {
+    console.log(formVal);
+  }
 }
