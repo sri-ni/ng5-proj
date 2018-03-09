c938ba5f946b663f8abe0c07405fe3b2a7bb2be9
Thirumalaa Srinivas <thrmlsrnvs@gmail.com>
2018-03-09 07:31:57 -0600

    Using RxJS streams and forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('formRef') formEx;
+
   title = 'Angular 5 Sandbox';
   username = 'Srini';
 
   onSubmit(formVal) {
     console.log(formVal);
+  }
+
+  ngAfterViewInit() {
+    this.formEx.valueChanges
+      .subscribe(v => console.log(v));
+
+    this.formEx.statusChanges
+      .subscribe(v => console.log(v));
+
+    Observable.combineLatest(
+      this.formEx.statusChanges,
+      this.formEx.valueChanges,
+      (status, value) => ({status, value})
+    )
+      .filter(({status}) => status === 'VALID')
+      .subscribe(({value}) => console.table(value));
   }
 }
